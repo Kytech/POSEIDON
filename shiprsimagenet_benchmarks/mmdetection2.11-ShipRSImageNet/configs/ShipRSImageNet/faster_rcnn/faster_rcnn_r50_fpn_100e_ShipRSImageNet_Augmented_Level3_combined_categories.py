73e6267e3f09f17d96c756eb1596dbde83d4627a
Kyler Nelson <kyler.nelson@usu.edu>
2024-09-28 14:34:21 -0600

    Add configurations for running training faster_rcnn on augmented ShipRSImageNet datasets.

---
+++ shiprsimagenet_benchmarks/mmdetection2.11-ShipRSImageNet/configs/ShipRSImageNet/faster_rcnn/faster_rcnn_r50_fpn_100e_ShipRSImageNet_Augmented_Level3_combined_categories.py
@@ -0,0 +1,15 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/ShipRSImageNet_Augmented_Level3_combined_categories_detection.py',
+    '../_base_/schedules/schedule_100e.py', '../_base_/default_runtime.py',
+]
+
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(
+            num_classes=10))
+    )
+
+
+evaluation = dict(interval=50, metric='bbox')
